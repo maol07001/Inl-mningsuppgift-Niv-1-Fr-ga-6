8430a84b0e985029db2ffef682168b840af7b490
mariu <mariu@DESKTOP-CT70FK7>
2024-12-16 16:35:26 +0100

    Add project files.

---
+++ Inlämningsuppgift-Nivå 1 Fråga 6/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace uppgift
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 10; i >= 1; i--)
+            {
+                Console.WriteLine(i); 
+            }
+        }
+    }
+}
